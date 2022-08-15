98860b5be69c8317f345b66809c985a8855935fd
Raito Bezarius <masterancpp@gmail.com>
2022-08-15 18:30:03 +0200

    ethash: 0.8.0 -> 0.9.0
    Re-enable tests as they work now.

--- pkgs/development/libraries/ethash/default.nix
+++ pkgs/development/libraries/ethash/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "ethash";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src =
     fetchFromGitHub {
       owner = "chfast";
       repo = "ethash";
       rev = "v${version}";
-      sha256 = "sha256-4SJk4niSpLPjymwTCD0kHOrqpMf+vE3J/O7DiffUSJ4=";
+      sha256 = "sha256-aXkgcZ8H2p30bQc2PB6AlyI0g3GXAbxRZE46AcgMEJ0=";
     };
 
   nativeBuildInputs = [
@@ -21,27 +21,20 @@ stdenv.mkDerivation rec {
     gtest
   ];
 
-  #preConfigure = ''
-  #  sed -i 's/GTest::main//' test/unittests/CMakeLists.txt
-  #  cat test/unittests/CMakeLists.txt
-  #  ln -sfv ${gtest.src}/googletest gtest
-  #'';
+  doCheck = true;
 
-  # NOTE: disabling tests due to gtest issue
   cmakeFlags = [
     "-DHUNTER_ENABLED=OFF"
-    "-DETHASH_BUILD_TESTS=OFF"
-    #"-Dbenchmark_DIR=${gbenchmark}/lib/cmake/benchmark"
-    #"-DGTest_DIR=${gtest.dev}/lib/cmake/GTest"
-    #"-DGTest_DIR=${gtest.src}/googletest"
-    #"-DCMAKE_PREFIX_PATH=${gtest.dev}/lib/cmake"
+    "-DETHASH_BUILD_TESTS=ON"
+    "-Dbenchmark_DIR=${gbenchmark}/lib/cmake/benchmark"
+    "-DGTest_DIR=${gtest.dev}/lib/cmake/GTest"
   ];
 
   meta = with lib; {
     description = "PoW algorithm for Ethereum 1.0 based on Dagger-Hashimoto";
     homepage = "https://github.com/ethereum/ethash";
     platforms = platforms.unix;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ raitobezarius ];
     license = licenses.asl20;
   };
 }
