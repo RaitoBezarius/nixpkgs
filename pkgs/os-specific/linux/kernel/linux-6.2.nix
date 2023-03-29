e1752e4016fb29cc5cdc9cf64f838b3836da8c67
Raito Bezarius <masterancpp@gmail.com>
2023-03-29 14:13:21 +0200

    linux_6_2: please export those symbols

--- pkgs/os-specific/linux/kernel/linux-6.2.nix
+++ pkgs/os-specific/linux/kernel/linux-6.2.nix
@@ -1,4 +1,4 @@
-{ lib, buildPackages, fetchurl, perl, buildLinux, nixosTests, ... } @ args:
+{ lib, buildPackages, fetchurl, perl, buildLinux, nixosTests, fetchpatch, ... } @ args:
 
 with lib;
 
@@ -15,4 +15,14 @@ buildLinux (args // rec {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
     sha256 = "138dpmj8qr5fcji99kmi3sj34ah21bgqgzsz2lbhn37v059100s3";
   };
+
+  kernelPatches = args.kernelPatches ++ [({
+    name = "export-neon-symbols-as-gpl";
+    patch = fetchpatch {
+      url = "https://github.com/torvalds/linux/commit/aaeca98456431a8d9382ecf48ac4843e252c07b3.patch";
+      hash = "sha256-L2g4G1tlWPIi/QRckMuHDcdWBcKpObSWSRTvbHRIwIk=";
+      revert = true;
+    };
+  })
+  ];
 } // (args.argsOverride or { }))
